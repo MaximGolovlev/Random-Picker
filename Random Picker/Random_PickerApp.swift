292ccb1bb0acec927171a7b6ce8fafa83e78cbf5
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-08-20 17:19:41 +0300

    Initial Commit

---
+++ Random Picker/Random_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_PickerApp.swift
+//  Random Picker
+//
+//  Created by Maxim Golovlev on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Random_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
